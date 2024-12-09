0825f1ea6a11316c3bd33157b4e263189b1133d6
AnnaNomokonova <annanomokon25@gmail.com>
2024-12-09 18:01:13 +0300

    Create utility.hpp

---
+++ hw2/utility.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace ILoveProgramming {
+    void generate_arrange(int * const x, const int size, const int depth);
+    void set_rand();
+    int random_int(const int depth);
+
+}
