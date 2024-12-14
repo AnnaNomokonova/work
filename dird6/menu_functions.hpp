12fa7c0ee0e8c6e57d7d3f695da3ec88c7be8138
AnnaNomokonova <annanomokon25@gmail.com>
2024-12-14 19:56:35 +0300

    Add files via upload

---
+++ dird6/menu_functions.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include "menu.hpp"
+
+namespace AnnaN {
+const MenuItem*  show_menu(const MenuItem* current);
+
+const MenuItem*  exit(const MenuItem* current);
+
+const MenuItem* read_pushkin(const MenuItem* current);
+const MenuItem* read_lermontow(const MenuItem* current);
+const MenuItem* read_krylow(const MenuItem* current);
+const MenuItem* read_go_back(const MenuItem* current);
+
+const MenuItem* read_pushkin_dubrovsky(const MenuItem* current);
+const MenuItem* read_pushkin_evgeny_onegin(const MenuItem* current);
+const MenuItem* read_pushkin_zimny_vecher(const MenuItem* current);
+const MenuItem* read_pushkin_go_back(const MenuItem* current);
+}
---
+++ dird6/menu_items.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "menu.hpp"
+namespace AnnaN {
+extern const MenuItem MAIN;
+
+extern const MenuItem READ;
+extern const MenuItem EXIT;
+
+extern const MenuItem READ_PUSHKIN;
+extern const MenuItem READ_LERMONTOW;
+extern const MenuItem READ_KRYLOW;
+extern const MenuItem READ_GO_BACK;
+
+extern const MenuItem READ_PUSHKIN_DUBROVSKY;
+extern const MenuItem READ_PUSHKIN_EVGENY_ONEGIN;
+extern const MenuItem READ_PUSHKIN_ZIMNY_VECHER;
+extern const MenuItem READ_PUSHKIN_GO_BACK;
+}
+
