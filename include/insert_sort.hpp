6591e18909b502d11d5fc266ffed01c2245dbd13
AnnaNomokonova <annanomokon25@gmail.com>
2025-03-05 23:28:53 +0300

    Delete include directory

--- include/insert_sort.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef INSERT_SORT_HPP
-#define INSERT_SORT_HPP
-
-namespace ILoveProgramming{
-    void insert_sort(int A[], int size);
-}
-#endif
--- include/randArray.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef RANDARRAY_HPP
-#define RANDARRAY_HPP
-
-namespace ILoveProgramming{
-    void rand_array(int* arr, int size, double max);
-}
-#endif
