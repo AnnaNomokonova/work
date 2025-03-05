f8df9ebb74600aa8b74ef365051e5947dc8503e8
AnnaNomokonova <annanomokon25@gmail.com>
2025-03-05 23:27:22 +0300

    Delete build_ninja directory

--- build_ninja/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anna/proga/work/tests
-# Build directory: /home/anna/proga/work/build_ninja/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(SortingTest "/home/anna/proga/work/build_ninja/tests/sorting_test")
-set_tests_properties(SortingTest PROPERTIES  _BACKTRACE_TRIPLES "/home/anna/proga/work/tests/CMakeLists.txt;13;add_test;/home/anna/proga/work/tests/CMakeLists.txt;0;")
